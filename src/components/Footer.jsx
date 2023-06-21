42cb95faf2ac29eb3640c4564b46822d2b858056
jkkariuki <jkkariuki15@gmail.com>
2023-06-21 14:57:29 -0400

    add color scheme. remove container from navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
-    <div class="container">
+    <div class="footer">
       <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div class="col-md-4 d-flex align-items-center">
           <a
             href="https://github.com/jkkariuki"
-            class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+            class=" mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
           >
             <FontAwesomeIcon className="icons" icon={faGithub} size="2x" />
           </a>
-          <span class="mb-3 mb-md-0 text-muted">
+          <span class="footerSpan mb-3 mb-md-0 text-muted">
             © 2023 JKCodes - James Kariuki
           </span>
         </div>
